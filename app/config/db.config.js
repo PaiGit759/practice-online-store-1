fb44fbc7bdd46acac2336186bba37cbdb3e1e5d5
PaiGit759 <107637697+PaiGit759@users.noreply.github.com>
2023-01-08 15:20:17 +0100

    20230108

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-    url: "mongodb://localhost:27017/online_store_db"
+// module.exports = {
+//     url: "mongodb://localhost:27017/online_store_db"
+//   };
+
+ 
+  module.exports = {
+    HOST: "localhost",
+    PORT: 27017,
+//    DB: "bezkoder_db"
+    DB: "online_store_db"
+
   };
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const dbConfig = require("./app/config/db.config");
+
 //const app = express();
-const fs = require('fs');
+//const fs = require('fs');
 
 //
 const path = __dirname + '/app/views/';
@@ -37,12 +39,15 @@ app.post("/download", (req, res) => {
   console.log('*****', req.body);
 //  res.json({ message: "*****Practical work" });
   res.send('9999')
-}); 
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 
 require("./app/routes/tutorial.routes")(app);
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
@@ -50,19 +55,96 @@ app.listen(PORT, () => {
 
 
 const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
+
+const Role = db.role;
+
+
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//     console.log("!",Role);
+//   //  initial();
+//   })
+//   .catch((err) => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
+ 
+
+/* db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
   .then(() => {
-    console.log("Connected to the database!");
+    console.log("Successfully connect to MongoDB.");
+  //  console.log("!",Role);
+    initial();
   })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  }); */
+
+  db.mongoose
+  .connect(`mongodb+srv://pai:MongoPai789@paipractice9.mk7qxu7.mongodb.net/test`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+  //  console.log("!",Role);
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
     process.exit();
   });
- 
+
+
+   function initial() {
+   // console.log("!",Role);
+     Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'user' to roles collection");
+        });
+  
+        new Role({
+          name: "moderator"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'moderator' to roles collection");
+        });
+  
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  } 
+  
+
+
 
 //node server.js
 //http://localhost:8080/
@@ -80,3 +162,16 @@ db.mongoose
 //npm start
 //
 //npm run build
+
+////http://localhost:8080/api/auth/signup
+
+// {
+//   "username" : "pai"
+//   "email" : "panig789@gmail.com"
+//   "password" : "Pai321789"
+//   "roles" : ["admin"]
+//   }
+
+//MongoDb Atlas pai MongoPai789
+
+//mongodb+srv://pai:MongoPai789@paipractice9.mk7qxu7.mongodb.net/test
